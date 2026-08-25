1a8330fe04290ea46e35642b2bd35463890311cc
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-25 13:14:37 +0545

    employee module completed

--- my-nest-app/src/employee/employee.controller.ts
+++ my-nest-app/src/employee/employee.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
+import { EmployeeService } from './employee.service';
 
 @Controller('employee')
-export class EmployeeController {}
+export class EmployeeController {
+  constructor(private readonly empService: EmployeeService) {}
+  @Get('/')
+  getAllEmps() {
+    return this.empService.getAllEmps();
+  }
+
+  @Get(':id')
+  getEmpById(@Param('id') id: string) {
+    return this.empService.getEmpById(Number(id));
+  }
+}
--- my-nest-app/src/employee/employee.service.ts
+++ my-nest-app/src/employee/employee.service.ts
@@ -1,4 +1,33 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    private employees = [
+        {
+
+            id:12,
+            name: "naresh bohara",
+            address:"Dhangadhi",
+            course:"NestJs"
+        },
+         {
+            id:13,
+            name: "hemraj ojha",
+            address:"Dhangadhi",
+            course:"Datascience"
+        },
+         {
+            id:14,
+            name: "dammar bhandari",
+            address:"Dhangadhi",
+            course:"QA"
+        },
+    ]
+
+    getAllEmps = ()=>this.employees;
+
+    getEmpById = (id:number)=>{
+        return this.employees.find((emp)=>emp.id===id)
+    }
+}
