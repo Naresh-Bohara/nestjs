7a52af7d7f44810847ca0b38b611daa27c593d2e
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-08-30 12:42:47 +0545

    get and post method added

--- my-nest-app/src/student/student.service.ts
+++ my-nest-app/src/student/student.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-empty */
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  private students = [
+    {
+      id: 1,
+      name: 'Naresh Bohara',
+      age: 25,
+    },
+    {
+      id: 2,
+      name: 'Hem Raj Ojha',
+      age: 24,
+    },
+  ];
+
+  getAllStudents = () => this.students;
+
+  getStudentById = (id: number) => {
+    const student = this.students.find((s) => s.id === id);
+    if (!student) throw new NotFoundException('Student not Found!');
+    return student;
+  };
+
+  //post method:
+  createStudent = (data: { name: string; age: number }) => {
+    const newStudent = {
+      id: Date.now(),
+      ...data,
+    };
+
+    this.students.push(newStudent);
+    return newStudent;
+  };
+
+  // put method:
+  
+}
