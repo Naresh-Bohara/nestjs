a401af3844b695bb6a0ae95b983bbf8da5d56425
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-05 12:30:18 +0545

    updated

--- my-nest-app/src/user-roles/user-roles.controller.ts
+++ my-nest-app/src/user-roles/user-roles.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/guards/roles/roles.decorator';
+import { Role } from 'src/guards/roles/roles.enums';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
 
 @Controller('user-roles')
-export class UserRolesController {}
+export class UserRolesController {
+  @Get('admin-data')
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
+  getAdminData() {
+    return {
+      messagge: 'Only admin can access..',
+    };
+  }
+
+  @Get('user-data')
+  getUserData(){
+    return {
+        message: "anyone can access"
+    }
+  }
+}
